5d041aafc92b2d3ee59c3cc92b6fa0107b56c4a3
Aliya <israilova.aliya13@gmail.com>
2026-07-04 14:03:58 +0600

    Добавлены модели: кастомный User с аватаркой, Theme, Answer; настроены медиа-файлы

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        verbose_name='Аватар'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Theme(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    content = models.TextField(verbose_name='Содержимое')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='themes',
+        verbose_name='Автор'
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def answers_count(self):
+        return self.answers.count()
+
+
+class Answer(models.Model):
+    theme = models.ForeignKey(
+        Theme,
+        on_delete=models.CASCADE,
+        related_name='answers',
+        verbose_name='Тема'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='answers',
+        verbose_name='Автор'
+    )
+    content = models.TextField(verbose_name='Содержимое')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.author.username} - {self.theme.title}'
